0e3f887f2557a0079992462b2fe2eb9b64513de5
Allen <Allenpmraz@gmail.com>
2026-04-21 14:43:37 -0400

    Got the main structer for TaskEntryScreen.kt set

--- app/src/main/java/com/example/notetakingapp/ui/task/TaskEntryScreen.kt
+++ app/src/main/java/com/example/notetakingapp/ui/task/TaskEntryScreen.kt
@@ -2,17 +2,67 @@ package com.example.notetakingapp.ui.task
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notetakingapp.R
+import com.example.notetakingapp.data.Task
+import kotlinx.coroutines.launch
+
+@Composable
+fun TaskEntryScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    canNavigateBack: Boolean = true,
+    viewModel: TaskEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TaskTopAppBar(
+                title = stringResource(TaskEntryDestination.titleRes),
+                canNavigateBack = canNavigateBack,
+                onNavigateUp = onNavigateUp
+            )
+        }
+    ) { innerPadding ->
+        TaskEntryBody(
+            taskUiState = viewModel.taskUiState,
+            onTaskValueChange = viewModel::updateUiState,
+            onSaveClick ={
+                coroutineScope.launch{
+                    viewModel.saveTask()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding()
+                )
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
 
 @Composable
 fun TaskEntryBody(
@@ -48,5 +98,35 @@ fun TaskInputForm(
     onValueChange: (TaskDetails) -> Unit= {},
     enabled: Boolean = true
 ){
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+    ){
+        OutlinedTextField(
+            value = taskDetails.title,
+            onValueChange = {onValueChange(taskDetails.copy(title = it))},
+            label = {Text(stringResource(R.string.task_title_req))},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = taskDetails.content,
+            onValueChange = {onValueChange(taskDetails.copy(content = it))},
+            label = {Text(stringResource(R.string.task_content_req))},
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
 }
